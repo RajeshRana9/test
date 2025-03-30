ac17f0e143bfb14f564243b7d0cb013a65290bb5
Rajesh Rana <rrana9882@gmail.com>
2025-03-30 23:32:26 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e  # Exit on error
 sudo apt-get update
-sudo apt-get install -y libxml2-dev libxslt1-dev zlib1g-dev
+sudo apt-get install -y \
+    libxml2-dev \
+    libxslt1-dev \
+    zlib1g-dev \
+    libgl1-mesa-glx
